aee795df9bb398cba4c7633649171d2e4ed7290c
TA\VICTUS <takub1148@gmail.com>
2026-05-18 10:59:00 +0700

    no message

--- src/features/admin/vms/overall/components/MapPanelVms.tsx
+++ src/features/admin/vms/overall/components/MapPanelVms.tsx
@@ -6,10 +6,6 @@ import styles from './vms.module.css'
 import MediaLibraryVms from './MediaLibraryVms'
 import { useVmsContext } from '../context'
 
-<<<<<<< HEAD
-
-=======
->>>>>>> bf69e86aaf18dfc6bad563701eca25cc01f15451
 const MapPanelVms: React.FC = () => {
   const { mediaExpanded } = useVmsContext()
   const [viewState, setViewState] = useState({
@@ -28,11 +24,7 @@ const MapPanelVms: React.FC = () => {
           {...viewState}
           onMove={(evt: any) => setViewState(evt.viewState)}
           mapStyle="mapbox://styles/mapbox/dark-v11"
-<<<<<<< HEAD
           mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN || ""}
-=======
-          mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN || ""}
->>>>>>> bf69e86aaf18dfc6bad563701eca25cc01f15451
           style={{ width: '100%', height: '100%' }}
         >
           <Layer
