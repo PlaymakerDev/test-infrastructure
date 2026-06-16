56a4a32346c505845e82f7123c30506a3fac6f81
Playmaker <44343329+PlaymakerDev@users.noreply.github.com>
2026-06-16 20:34:29 +0700

    feat: Add new modal components and weather constants, implement VMS detail and temperature charts

---
+++ src/features/admin/vms/detail/components/sections/overall/ChartContent.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { WeatherChart, TemperatureChart } from '../../../components'
+import { APIResponseVMSDetail } from '@/types/vms/detail-api'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/pagination'
+
+interface Props {
+  data?: APIResponseVMSDetail
+}
+
+const ChartContent: React.FC<Props> = (props) => {
+  const { data } = props
+  console.log(">>>>>>>>>", data)
+  return (
+    <Swiper
+      modules={[Pagination]}
+      pagination={{ clickable: true }}
+      autoHeight
+      className='w-full'
+    >
+      <SwiperSlide className='bg-transparent! pb-7'>
+        <WeatherChart data={data} />
+      </SwiperSlide>
+      <SwiperSlide className='bg-transparent! pb-7'>
+        <TemperatureChart data={data} />
+      </SwiperSlide>
+    </Swiper>
+  )
+}
+
+export default React.memo<Props>(ChartContent)
