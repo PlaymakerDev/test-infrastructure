962f2a1ed182b58cad002534163215f7948ee337
Playmaker <44343329+PlaymakerDev@users.noreply.github.com>
2026-07-20 11:02:25 +0700

    refactor: comment out unused SummarySection and related options in TitleSection

--- src/features/admin/tracking/detail/gps/components/sections/overall/MapOverallSection.tsx
+++ src/features/admin/tracking/detail/gps/components/sections/overall/MapOverallSection.tsx
@@ -10,14 +10,14 @@ const MapOverallSection: React.FC<Props> = (props) => {
   const { } = props
 
   return (
-    <div className='h-200'>
-      {/* <BaseMap
+    <div className='relative w-full h-202 rounded-lg overflow-hidden'>
+      <BaseMap
         initialCenter={[101.0, 14.5]}
         initialZoom={5.4}
-        edgeFade={{ left: 10, right: 10, top: 10, bottom: 10 }}
+        edgeFade={{ all: 10 }}
       >
         <ThailandMaskLayer />
-      </BaseMap> */}
+      </BaseMap>
     </div>
   )
 }
